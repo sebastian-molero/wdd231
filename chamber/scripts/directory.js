6cdd5eca6fac9af3af4010b6542c1db993e0cc32
sebastian-molero <smoleronavarro@byupathway.edu>
2025-09-11 23:42:06 -0500

    Week 02

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,82 @@
+const hamburger = document.getElementById('ham-Btn');
+const navigation = document.getElementById('nav-bar');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('show');
+    navigation.classList.toggle('show');
+});
+
+
+const directory = document.getElementById('directory');
+const gridBtn = document.getElementById('gridViewBtn');
+const listBtn = document.getElementById('listViewBtn');
+
+const membersJson = "data/members.json";
+
+async function getMembers(membersJson) {
+    const response = await fetch(membersJson);
+    const data = await response.json();
+    displayMembers(data.members);
+};
+
+const displayMembers = (members => {
+    directory.innerHTML = "";
+
+    members.forEach(member => {
+        const card = document.createElement('section');
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+        const address = document.createElement('p');
+        const contact = document.createElement('p');
+        const website = document.createElement('p');
+
+        card.classList.add('member-card');
+
+        if (card.membership === 3) {
+            card.classList.add('gold');
+        }
+        else if (card.membership === 2) {
+            card.classList.add('silver');
+        }
+        else {
+            card.classList.add('regular');
+        }
+
+        img.setAttribute('src', `${member.image}`);
+        img.setAttribute('alt', `${member.name}`);
+        img.setAttribute('loading', 'lazy');
+
+        h2.textContent = `${member.name}`;
+        address.innerHTML = `<strong>Address:</strong> ${member.address}`;
+        contact.innerHTML = `<strong>Phone Number:</strong><br> ${member.phone}`;
+        website.innerHTML = `<strong>Website: </strong><a href="${member.website}" target="_blank">${member.name}</a>`;
+
+        card.appendChild(img);
+        card.appendChild(h2);
+        card.appendChild(address);
+        card.appendChild(contact);
+        card.appendChild(website);
+
+        directory.appendChild(card);
+    });
+})
+
+gridBtn.addEventListener('click', function () {
+    directory.classList.add('directory-grid');
+    directory.classList.remove('directory-list');
+    gridBtn.classList.add('active');
+    listBtn.classList.remove('active');
+});
+
+listBtn.addEventListener('click', function () {
+    directory.classList.add('directory-list');
+    directory.classList.remove('directory-grid');
+    listBtn.classList.add('active');
+    gridBtn.classList.remove('active');
+})
+
+
+getMembers(membersJson)
+
+document.getElementById('year').textContent = new Date().getFullYear()
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`
