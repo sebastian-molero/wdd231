13427da52c735e4de6f6a8f0a8c7f9c48dfd3c57
sebastian-molero <smoleronavarro@byupathway.edu>
2025-10-11 14:46:16 -0500

    Final Project

--- final_project/scripts/thankyou.js
+++ final_project/scripts/thankyou.js
@@ -5,6 +5,18 @@ document.getElementById('email').textContent = params.get('email') || 'Not Provi
 document.getElementById('phone').textContent = params.get('phone') || 'Not Provided';
 document.getElementById('location').textContent = params.get('location') || 'Not Provided';
 document.getElementById('reason').textContent = params.get('reason') || 'Not Provided';
-document.getElementById('petName').textContent = params.get('petName') || 'Not Provided';
+
+const petNameP = document.getElementById('petNameP');
+const petName = document.getElementById('petName');
+
+const petNameValue = params.get('petName');
+
+if (!petNameValue || petNameValue.trim() === "") {
+    petNameP.style.display = "none";
+} else {
+    petName.textContent = petNameValue;
+    petNameP.style.display = "block";
+}
+
 document.getElementById('timestamp').textContent = params.get('timestamp') || 'Not Available';
 
